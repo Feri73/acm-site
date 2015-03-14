afd7d99ff0d7a5b90ba14fb71edee302e35ef9ad
Feri73 <ferisampad@gmail.com>
2015-03-14 09:19:48 +0330

    root?

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,4 @@
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
-
+RAILS_ROOT = "#{File.dirname(__FILE__)}/acm" unless defined?(RAILS_ROOT)
 require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
