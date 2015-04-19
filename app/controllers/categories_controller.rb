3b7f6ef79d80ca06a98f9523fbfdfa789a6f1fbc
Feri73 <ferisampad@gmail.com>
2015-04-19 19:19:03 +0430

    categories access

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
   before_action :set_category, only: [:show, :edit, :update, :destroy]
 
   # GET /categories
@@ -27,6 +27,7 @@ class CategoriesController < ApplicationController
 
   # GET /categories/new
   def new
+    authorize_user
     @category = Category.new
   end
 
