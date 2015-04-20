0e37d4af2f3ec7d40aa1b8393bcf58ed8532a26d
Feri73 <ferisampad@gmail.com>
2015-04-20 13:21:16 +0430

    pages

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,7 @@ class PostsController < ApplicationController
     # @posts = Post.order('publishDate DESC').limit(10)
     first=(params[:first].to_i ||= 0) 
     last=(params[:last].to_i ||= 10) 
-    @posts = Post.order(:publishDate).reverse().to_a[(first..last)]
+    @posts = Post.order(:publishDate).reverse().to_a[first..last]
     # if params.has_key?(:english)
     #   @eng= (params[:english]=="1")
     # else
