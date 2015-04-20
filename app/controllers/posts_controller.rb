6eca2735fe1e7cf540679f5ffd65456d9aa1d7ba
Feri73 <ferisampad@gmail.com>
2015-04-20 13:26:46 +0430

    pages

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,10 +5,8 @@ class PostsController < ApplicationController
   # GET /posts.json
   def index
     # @posts = Post.order('publishDate DESC').limit(10)
-    first=(params[:first] ||= "0") 
-    last=(params[:last] ||= "10")
-    first=first.to_i
-    last=last.to_i
+    first=(params[:first] ? params[:first].to_i : 0) 
+    last=(params[:last] ? params[:last].to_i : 10) 
     logger.info "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
     logger.info first
     logger.info last 
