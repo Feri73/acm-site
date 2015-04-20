60669f8b4c37d068ebb85ed086430c0998b08aa6
Feri73 <ferisampad@gmail.com>
2015-04-20 15:48:46 +0430

    stringToText and locale update

--- db/migrate/20150420111008_string_to_text.rb
+++ db/migrate/20150420111008_string_to_text.rb
@@ -1,4 +1,7 @@
 class StringToText < ActiveRecord::Migration
   def change
+    change_column :posts, :englishText, :text
+    change_column :posts, :englishTitle, :text
+    change_column :posts, :englishSummary, :text
   end
 end
